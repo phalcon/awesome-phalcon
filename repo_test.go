db0d8f95d706dcff2420de8f8d1f33c9103ba3b9
Serghei Iakovlev <sadhooklay@gmail.com>
2015-06-02 02:09:25 +0300

    Added copyright notice

--- repo_test.go
+++ repo_test.go
@@ -1,5 +1,12 @@
 package repo
 
+/**
+ * This file licensed under MIT license.
+ * Copyright (c) 2015 Thiago Avelino
+ *
+ * Source: https://github.com/avelino/awesome-go
+ */
+
 import (
 	"bytes"
 	"io/ioutil"
